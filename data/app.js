b8a8a5e6830760d32fbdc8ae3007696d760fc56d
garcesgustavo <garcesgustavo@gmail.com>
2022-05-15 21:06:45 -0300

    correcciones

---
+++ data/app.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+import { preguntas } from "./preguntas";
+
+import { Quiz } from "../model/Quiz.js";
+
+import { UI } from "../model/UI.js";
+/**
+ *
+ * @param {Quiz} quiz objeto pricipal de las preguntas.
+ * @param {UI} ui objeto ui
+ */
+const reiniciar = (quiz, ui) => {
+  if (quiz.final()) {
+    ui.mostrarPuntaje(quiz.puntaje);
+  } else {
+    ui.mostrarPregunta(quiz.getPreguntaIndex().pregunta);
+    ui.mostrarOpciones(quiz.getPreguntaIndex().opciones, (opcionActual) => {
+      quiz.adivina(opcionActual);
+      reiniciar(quiz, ui);
+    });
+    ui.mostrarProgreso(quiz.preguntasIndex + 1, quiz.preguntas.length);
+  }
+};
+
+function principal() {
+  const quiz = new Quiz(preguntas);
+
+  const ui = new UI();
+  reiniciar(quiz, ui);
+}
+principal();
